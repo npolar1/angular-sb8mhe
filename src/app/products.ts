f52a6f70ae8527ff38f35ec3e62dbf461a1f3ee4
npolar1 <npolar1@gmail.com>
2020-11-10 03:17:00 -0300

    1 binaries uploaded

--- src/app/products.ts
+++ src/app/products.ts
@@ -1,21 +1,47 @@
 export const products = [
   {
-    name: 'Phone XL',
+    name: "Agustina Candioti",
     price: 799,
-    description: 'A large phone with one of the best screens'
+    description: "A large phone with one of the best screens",
+    bolson: 1,
+    miel: 0,
+    tinto: 0,
+    blanco: 0,
+    yerba: 0,
+    huevos: 0,
+    donacion: 0,
+    total: 900,
+    cel: "1136752230"
   },
   {
-    name: 'Phone Mini',
+    name: "Alejandra Pochettino",
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: "A great phone with one of the best cameras",
+    bolson: 1,
+    miel: 0,
+    tinto: 0,
+    blanco: 0,
+    yerba: 0,
+    huevos: 1,
+    donacion: 0,
+    total: 1200,
+    cel: "1167390100"
   },
   {
-    name: 'Phone Standard',
+    name: "Ana Lewin",
     price: 299,
-    description: ''
+    description: "",
+    bolson: 1,
+    miel: 0,
+    tinto: 0,
+    blanco: 0,
+    yerba: 0,
+    huevos: 1,
+    donacion: 100,
+    total: 1300,
+    cel: "1167896490"
   }
 ];
-
 
 /*
 Copyright Google LLC. All Rights Reserved.
